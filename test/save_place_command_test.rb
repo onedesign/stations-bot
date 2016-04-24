bf78f3237ef97076e14bd493341a9b1f570d7cb3
thelowlypeon <peter@onedesigncompany.com>
2016-04-24 12:25:09 -0500

    adds test to assert user is found or created and updated on requests

---
+++ test/save_place_command_test.rb
@@ -0,0 +1,10 @@
+require File.expand_path('../test_helper.rb', __FILE__)
+
+class SavePlaceCommandTest < StationsBotTestBase
+
+  def test_matches_save_placee_command
+    post '/v1/', slack_params.merge(text: 'set placename 1 n state st, 60657')
+    assert_response_format JSON.parse(last_response.body)
+  end
+
+end
