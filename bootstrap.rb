c9d980859812660c70825c200eb4499d9410e5a1
thelowlypeon <peter@onedesigncompany.com>
2016-04-24 15:11:14 -0500

    sorts commands so heroku can load them in order

--- bootstrap.rb
+++ bootstrap.rb
@@ -8,7 +8,7 @@ require 'geocoder'
 require File.expand_path('../db/connect.rb', __FILE__)
 
 Dir["./lib/*.rb"].each {|file| require file }
-Dir["./app/commands/*.rb"].each {|file| require file }
+Dir["./app/commands/*.rb"].sort.each {|file| require file }
 Dir["./app/models/*.rb"].each {|file| require file }
 
 require './stations_bot.rb'
