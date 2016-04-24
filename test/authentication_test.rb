dfab8787e7991aed21eeb77db06f75a4d81dcf46
thelowlypeon <peter@onedesigncompany.com>
2016-04-24 12:27:21 -0500

    adds test to assert user is found or created and updated on requests

--- test/authentication_test.rb
+++ test/authentication_test.rb
@@ -18,4 +18,23 @@ class AuthenticationTest < StationsBotTestBase
     refute last_response.status == 401
   end
 
+  def test_unrecognized_user_created_on_request
+    params = slack_params
+    params[:token] = ENV['SLACK_APPLICATION_TOKEN']
+    user_id = params[:user_id]
+    post '/v1/', params
+    user = User.where(user_id: user_id).first
+    refute user.nil?
+    assert_equal user.user_id, user_id
+  end
+
+  def test_recognized_user_found_on_request
+    user = User.create(user_id: random_string)
+    num_requests = user.num_requests
+    updated_at = user.updated_at
+    post '/v1/', slack_params.merge(user_id: user.user_id)
+    user.refresh
+    assert_equal num_requests + 1, user.num_requests
+    refute_equal updated_at, user.updated_at
+  end
 end
