ca0a3bc9f255460fb2b2bd1d31f3e02ec25ede4a
thelowlypeon <peter@onedesigncompany.com>
2016-04-26 15:54:55 -0500

    adds team_id to user

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,9 +3,10 @@ require 'rubygems'
 require 'minitest/autorun'
 require 'rack/test'
 
+File.delete('test/stations_bot-test.db')
+
 require File.expand_path('../../bootstrap.rb', __FILE__)
 require File.expand_path('../stations_bot_test_base.rb', __FILE__)
 
 Sequel.extension :migration
-Sequel::Migrator.run(DB, 'db/migrations', :target => 0)
 Sequel::Migrator.run(DB, 'db/migrations')
